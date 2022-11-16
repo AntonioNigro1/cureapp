38da49644797e122f3d51fe9bef014ee8d0090c7
AntonioNigro1 <toninigro@live.com>
2022-11-16 14:27:25 -0300

    removed web files
    removing web files from server branch

--- web/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import CallBoard from "./callBoard";
-
-export default function Home() {
-  return <CallBoard />;
-}
